1810d39e3ec393ae50e54b376e41aeb29441f83e
LQuocBao <lamquocbao26042005@gmail.com>
2024-11-19 16:11:47 +0700

    4:11 19/11

---
+++ admin/index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once 'app/view/menu.php';
+    require_once 'app/view/category.php';
+    require_once '../app/model/database.php';
+    // require_once 'app/'
+    $db = new Database();
